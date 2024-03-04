0d63e6eaebf586e59c0d61f977eb8a0cb1791170
Franco Canzani <francocanzani@gmail.com>
2024-03-04 23:02:06 +0100

    fix redirect uri mismatch

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
               <button
                 onClick={() =>
                   signOut({
-                    callbackUrl: `https://calories-chat.vercel.app/`,
+                    callbackUrl: `/`,
                   })
                 }
               >
@@ -28,7 +28,7 @@ export function Header() {
               <button
                 onClick={() =>
                   signIn('github', {
-                    callbackUrl: `https://calories-chat.vercel.app/chat`,
+                    callbackUrl: `/chat`,
                   })
                 }
               >
